c323a3b786a2ea5b63d0690752c6840507d0055d
mIINguta <gustavo.minguta@gmail.com>
2024-02-27 09:30:09 -0300

    feat: Section Imposto de Renda

---
+++ src/components/SectionImpostoRenda.tsx
@@ -0,0 +1,27 @@
+export default function SectionImpostoRenda(){
+    return(
+        <>
+            <section className="section-imposto-renda">
+                <h2>Documentos necessários para realizar o imposto de renda!</h2>
+                    <ol>
+                        <li><span>Documentos pessoais:</span> RG, CPF, comprovante de residência e dados bancários são essenciais para identificação e fornecem as informações necessárias para o correto preenchimento da declaração.</li>
+                        <li><span>Informe de Rendimentos:</span> a empresa deve fornecer o documento que detalha os rendimentos e descontos ao longo do ano anterior.</li>
+                        <li><span>Documentos pessoais dos dependentes:</span> CPF é obrigatório para cada dependente.</li>
+                        <li><span>Informe de Rendimentos financeiros e de aplicações:</span> fornecidos pelo banco, são cruciais para declarar os ganhos provenientes de investimentos.</li>
+                        <li><span>Comprovantes de despesas médicas</span>: devem incluir nome, endereço, CPF ou CNPJ do prestador, data e assinatura do médico, caso não seja uma nota fiscal.</li>
+                        <li><span>Comprovantes de despesas com ensino:</span> documentos que atestam gastos com educação, um importante item dedutível.</li>
+                        <li><span>Extrato de Previdência Privada:</span> detalha os valores investidos e rendimentos relativos à previdência privada.</li>
+                        <li><span>Documentação do plano de saúde:</span> comprovante dos pagamentos do plano de saúde, fundamental para deduções.</li>
+                        <li><span>Documentação de imóveis e veículos:</span> incluindo aqueles financiados, para declarar propriedades e veículos.</li>
+                        <li><span>Recibos de pagamento ou recebimento de aluguel:</span> caso você seja locador ou locatário, tenha os recibos em mãos.</li>
+                        <li><span>Recibos de doações</span>: documentação importante para quem realizou ou recebeu doações ao longo do ano.</li>
+                        <li><span>Contrato Social das empresas das quais é sóc</span>io: para quem possui participação em empresas, o contrato social é necessário.</li>
+                        <li><span>Documentação de consórcios contemplados ou não:</span> detalhes sobre consórcios, incluindo se foram contemplados ou não.</li>
+                        <li><span>Extrato do Carnê-Leão (caso autônomo):</span> essencial para autônomos, detalhando os rendimentos ao longo do ano.</li>
+                    </ol>
+            </section>
+
+        
+        </>
+    )
+}
